2fdb89c02ed0f09d95f25ce87a3979e61b9c3866
zachm-dku <108750150+zachm-dku@users.noreply.github.com>
2022-10-09 18:07:37 -0700

    Move param generation to a separate lib

--- custom-recipes/generate-images/recipe.py
+++ custom-recipes/generate-images/recipe.py
@@ -1,7 +1,5 @@
 import logging
 
-import dataiku
-import torch
 from dataiku.customrecipe import (
     get_input_names_for_role,
     get_output_names_for_role,
@@ -9,62 +7,37 @@ from dataiku.customrecipe import (
 )
 
 from ai_art.generate_image import ImageGenerator
+from ai_art.params import GenerateImagesParams
 
 weights_folder_name = get_input_names_for_role("weights_folder")[0]
 image_folder_name = get_output_names_for_role("image_folder")[0]
-weights_folder = dataiku.Folder(weights_folder_name)
-image_folder = dataiku.Folder(image_folder_name)
-
 recipe_config = get_recipe_config()
-prompt = recipe_config["prompt"]
-image_count = recipe_config["image_count"]
-batch_size = recipe_config["batch_size"]
-filename_prefix = recipe_config["filename_prefix"]
-device = recipe_config["device"]
-clear_folder = recipe_config["clear_folder"]
-image_height = recipe_config["image_height"]
-image_width = recipe_config["image_width"]
-use_autocast = recipe_config["use_autocast"]
-use_half_precision = recipe_config["use_half_precision"]
-
-# TODO: figure out if there's a better way to store the weights. The
-# current method only works if the folder is on the local FS.
-# https://huggingface.co/docs/diffusers/v0.3.0/en/api/diffusion_pipeline#diffusers.DiffusionPipeline.from_pretrained
-weights_path = weights_folder.get_path()
-
-if device == "auto":
-    device_id = None
-else:
-    device_id = device
-
-if use_half_precision:
-    torch_dtype = torch.float16
-else:
-    # Use the default dtype of the model (float32)
-    torch_dtype = None
+params = GenerateImagesParams.from_config(
+    recipe_config, weights_folder_name, image_folder_name
+)
 
 generator = ImageGenerator(
-    weights_path,
-    device_id=device_id,
-    torch_dtype=torch_dtype,
+    params.weights_path,
+    device_id=params.device_id,
+    torch_dtype=params.torch_dtype,
 )
 
-if clear_folder:
+if params.clear_folder:
     logging.info("Clearing image folder")
-    image_folder.clear()
+    params.image_folder.clear()
 
 images = generator.generate_images(
-    prompt,
-    image_count,
-    batch_size,
-    height=image_height,
-    width=image_width,
-    use_autocast=use_autocast,
+    params.prompt,
+    params.image_count,
+    params.batch_size,
+    height=params.image_height,
+    width=params.image_width,
+    use_autocast=params.use_autocast,
 )
 for i, image in enumerate(images):
-    filename = f"{filename_prefix}{i+1}.png"
+    filename = f"{params.filename_prefix}{i+1}.png"
 
     logging.info("Saving image: %s", filename)
-    with image_folder.get_writer(filename) as f:
+    with params.image_folder.get_writer(filename) as f:
         # TODO: make the format configurable
         image.save(f, format="PNG")
--- python-lib/ai_art/params.py
+++ python-lib/ai_art/params.py
@@ -1,6 +1,17 @@
+from __future__ import annotations
+
+import dataclasses
+import logging
+from typing import TYPE_CHECKING
 from urllib.parse import urljoin
 
+import dataiku
+import torch
+
 from ai_art.constants import HUGGING_FACE_BASE_URL
+
+if TYPE_CHECKING:
+    from typing import Optional
 
 
 def resolve_model_repo(config):
@@ -18,3 +29,64 @@ def resolve_model_repo(config):
 
     model_repo = urljoin(HUGGING_FACE_BASE_URL, model_repo_path)
     return model_repo
+
+
+@dataclasses.dataclass
+class GenerateImagesParams:
+    weights_path: str
+    image_folder: dataiku.Folder
+    prompt: str
+    image_count: int
+    batch_size: int
+    filename_prefix: str
+    device_id: Optional[str]
+    clear_folder: bool
+    image_height: int
+    image_width: int
+    use_autocast: bool
+    torch_dtype: Optional[torch.dtype]
+
+    @classmethod
+    def from_config(
+        cls, recipe_config, weights_folder_name, image_folder_name
+    ):
+        logging.info("Recipe config: %r", recipe_config)
+        logging.info("Weights folder: %r", weights_folder_name)
+        logging.info("Image folder: %r", image_folder_name)
+
+        weights_folder = dataiku.Folder(weights_folder_name)
+        image_folder = dataiku.Folder(image_folder_name)
+
+        # TODO: figure out if there's a better way to store the weights.
+        # The current method only works if the folder is on the local
+        # FS.
+        # https://huggingface.co/docs/diffusers/v0.3.0/en/api/diffusion_pipeline#diffusers.DiffusionPipeline.from_pretrained
+        weights_path = weights_folder.get_path()
+
+        device = recipe_config["device"]
+        if device == "auto":
+            device_id = None
+        else:
+            device_id = device
+
+        use_half_precision = recipe_config["use_half_precision"]
+        if use_half_precision:
+            torch_dtype = torch.float16
+        else:
+            # Use the default dtype of the model (float32)
+            torch_dtype = None
+
+        return cls(
+            weights_path=weights_path,
+            image_folder=image_folder,
+            prompt=recipe_config["prompt"],
+            image_count=recipe_config["image_count"],
+            batch_size=recipe_config["batch_size"],
+            filename_prefix=recipe_config["filename_prefix"],
+            device_id=device_id,
+            clear_folder=recipe_config["clear_folder"],
+            image_height=recipe_config["image_height"],
+            image_width=recipe_config["image_width"],
+            use_autocast=recipe_config["use_autocast"],
+            torch_dtype=torch_dtype,
+        )
